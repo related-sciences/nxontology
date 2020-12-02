86ca03ad24ff30777e5e7d95f38bfebf3fbbea2b
Daniel Himmelstein <dhimmel@related.vc>
2020-12-02 18:00:44 -0500

    setup long_description

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
+import pathlib
+
 from setuptools import setup
+
+directory = pathlib.Path(__file__).parent
+readme = directory.joinpath("README.md").read_text()
 
 setup(
     use_scm_version=True,
+    long_description=readme,
+    long_description_content_type="text/markdown",
 )
