509d6c6a6ad8f832291b812412ac9bac5fdaacb8
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2020-12-08 08:36:18 -0500

    Update packaging and setup configuration (#3)
    merges https://github.com/related-sciences/nxontology/pull/3
    
    * remove all args from setuptools.setup
    use pyproject.toml to specify build/setup dependencies
    use declarative config for long_description
    
    * pre-commit: remove seed-isort-config
    From https://github.com/asottile-archive/seed-isort-config:
    > DEPRECATED: this is no longer needed as of isort>=5
    
    * build package with pypa/build
    * remove wheel from dev dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-import pathlib
-
 from setuptools import setup
 
-directory = pathlib.Path(__file__).parent
-readme = directory.joinpath("README.md").read_text(encoding="utf-8")
-
-setup(
-    use_scm_version=True,
-    long_description=readme,
-    long_description_content_type="text/markdown",
-)
+setup()
